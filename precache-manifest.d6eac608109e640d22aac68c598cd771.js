ce4d4a29a5b175b5d5ef7ec69ef0ac79ce85cb6c
saurav529 <saurav.bhandari@yahoo.com>
2020-03-24 09:51:15 +0545

    Updates

---
+++ precache-manifest.d6eac608109e640d22aac68c598cd771.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f59dd9f88260e3ce88f6b9c3df948ace",
+    "url": "/react-social-login/index.html"
+  },
+  {
+    "revision": "a5623110f7ec2851ae18",
+    "url": "/react-social-login/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "db716a343c5038a5c5a5",
+    "url": "/react-social-login/static/css/main.c29f265a.chunk.css"
+  },
+  {
+    "revision": "a5623110f7ec2851ae18",
+    "url": "/react-social-login/static/js/2.a320c316.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-social-login/static/js/2.a320c316.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db716a343c5038a5c5a5",
+    "url": "/react-social-login/static/js/main.c3219d78.chunk.js"
+  },
+  {
+    "revision": "b3cbbfacfd1d185756e3",
+    "url": "/react-social-login/static/js/runtime-main.4fd4b3e1.js"
+  }
+]);
